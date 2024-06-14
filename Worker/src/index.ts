2aba09282d7db4cb3cac188606843246f71a0b44
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2024-06-14 18:48:42 +0530

    added

---
+++ Worker/src/index.ts
@@ -0,0 +1,39 @@
+import { createClient } from "redis";
+const client = createClient();
+
+async function processSubmission(submission: string) {
+    const { problemId, code, language } = JSON.parse(submission);
+
+    console.log(`Processing submission for problemId ${problemId}...`);
+    console.log(`Code: ${code}`);
+    console.log(`Language: ${language}`);
+    // Here you would add your actual processing logic
+
+    // Simulate processing delay
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log(`Finished processing submission for problemId ${problemId}.`);
+}
+
+async function startWorker() {
+
+    try {
+        await client.connect();
+        console.log("Worker connected to Redis.");
+
+        
+        while (true) {
+            try {
+                const submission = await client.brPop("problems", 0);// this line means it will run infinitely and as soon as it gets a submission it goes to the next line
+                console.log(submission)
+                // @ts-ignore
+                await processSubmission(submission.element);
+            } catch (error) {
+                console.error("Error processing submission:", error);
+            }
+        }
+    } catch (error) {
+        console.error("Failed to connect to Redis", error);
+    }
+}
+
+startWorker();
